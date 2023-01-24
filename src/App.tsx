ade45b8452279ba2eccecaeea373b73cb07aa24d
rayan-saleh <86924567+rayan-saleh@users.noreply.github.com>
2023-01-24 17:25:02 +0000

    ttt basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Board from './components/Board'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Board />
     </div>
   )
 }
